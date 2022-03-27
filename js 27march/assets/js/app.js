ae3d3fc344f00ff305cca426c3b7d060822712fc
Seema Ahmed <72159369+webprogrammer124@users.noreply.github.com>
2022-03-27 14:28:35 +0500

    sunday

---
+++ js 27march/assets/js/app.js
@@ -0,0 +1,9 @@
+// var months = ["JaNuaRy","FeBruAry","MaRcH","aPrIl","MaY","jUNe"]
+// var monthsUpperCase = []
+// for(i=0; i< months.length; i++){
+//     monthsUpperCase.push(months[i].toUpperCase())
+// }
+// console.log(monthsUpperCase)
+var _name = "momina"
+_name.slice(0,1).toUpperCase()
+console.log(_name)
