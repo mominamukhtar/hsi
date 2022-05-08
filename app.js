e005be9aca88df72962896fa78d27ff3e5db110f
maiya2 <ss5706817@gmail.com>
2022-05-08 13:49:01 +0500

    editt

---
+++ app.js
@@ -0,0 +1,21 @@
+// function Tax(salary){
+//  deducttax = salary *0.10
+//  return salary - deducttax 
+// }
+// Tax(1000)
+// function expanse(salary){
+//     return salary - 1000
+// }
+//  expanse(10000)
+//  expanse(Tax(10000))
+// console.log(Tax(1000))
+
+function billpay(){
+    var foods = 0
+    foods = foods + 750
+    foods = foods + 1075
+    foods = foods + 2000
+    foods = foods +5000
+    return foods
+}
+console.log(billpay())
